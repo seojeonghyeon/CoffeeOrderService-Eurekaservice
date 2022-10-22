7a31685abee39e448453b82a3c85060171b97433
seojeonghyeon <seojeonghyeon0630@gmail.com>
2022-10-23 00:32:44 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CoffeeOrderService-Eurekaservice
+ CoffeeOrderService-Eurekaservice
---
+++ src/test/java/com/zayden/coffeeorderserviceeurekaservice/CoffeeOrderServiceEurekaserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zayden.coffeeorderserviceeurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeOrderServiceEurekaserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
